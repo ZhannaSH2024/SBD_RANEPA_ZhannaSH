ea377ec9559cebc03e173207a5087ff4f7e002b5
ZhannaSH2024 <shuvalovazh4@gmail.com>
2024-11-12 18:16:36 +0000

    CW 12_11 dz8

--- orange_d005.py
+++ orange_d005.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 # Четвертый узел DAG
 @click.command()
@@ -22,7 +23,23 @@ def run(infile, outfile):
 # Байесовский классификатор
     nb_classifier = GaussianNB()
     nb_classifier.fit(X_train, y_train)
-    joblib.dump(df, outfile)
+
+    nb_predictions = nb_classifier.predict(X_test)
+
+    accuracy = accuracy_score(y_test, nb_predictions)
+    print(f"Точность Байесовского классификатора: {accuracy:.4f}")
+    conf_matrix = confusion_matrix(y_test, nb_predictions)
+
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(conf_matrix, annot=True, fmt='d', cmap='Blues', 
+            xticklabels=['Adelie', 'Chinstrap', 'Gentoo'],
+            yticklabels=['Adelie', 'Chinstrap', 'Gentoo'])
+    plt.title('Матрица ошибок (Confusion Matrix)')
+    plt.xlabel('Предсказано')
+    plt.ylabel('Фактическое')
+    plt.show()
+
+    joblib.dump(nb_predictions, outfile)
     click.echo(f"Создан файл: {outfile}!")
 
 if __name__ == '__main__':
